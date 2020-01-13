204ae626d8445a58825f5cea88f3b7ae2ef6bf98
mmazepa <maksymilianmariusz@gmail.com>
2020-01-13 01:37:43 +0100

    Wczytywanie grafu z pliku.

--- 10_planarity/src/main/java/GraphManager.java
+++ 10_planarity/src/main/java/GraphManager.java
@@ -40,33 +40,4 @@ public class GraphManager {
       System.out.print("\n");
     }
   }
-
-  public static void breakLine() {
-    System.out.print("\n");
-  }
-
-  public static void frameIt(String text, Boolean bold) {
-    System.out.print(bold ? "   ╔═" : "   ╭─");
-    for (int i = 0; i < text.length(); i++) System.out.print(bold ? "═" : "─");
-    System.out.print(bold ? "═╗\n" : "─╮\n");
-    System.out.print(bold ? "   ║ " + text + " ║\n" : "   │ " + text + " │\n");
-    System.out.print(bold ? "   ╚═" : "   ╰─");
-    for (int i = 0; i < text.length(); i++) System.out.print(bold ? "═" : "─");
-    System.out.print(bold ? "═╝\n" : "─╯\n");
-  }
-
-  public static void horizontalLine(int length) {
-    String segment = "╌◇╌◆";
-    int counter = 0;
-    int segLen = segment.length();
-
-    System.out.print("   ▸");
-    while ((length-2) > 0) {
-      System.out.print(segment.charAt(counter % segLen));
-      length--;
-      counter++;
-    }
-    System.out.print("◂");
-    breakLine();
-  }
 }
--- 10_planarity/src/test/java/PlanarTest.java
+++ 10_planarity/src/test/java/PlanarTest.java
@@ -1,5 +1,5 @@
-import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import java.io.File;
 
 import org.jgrapht.*;
 import org.jgrapht.graph.*;
@@ -8,35 +8,31 @@ import org.jgrapht.alg.planar.*;
 public class PlanarTest {
 
   public static GraphManager gm = new GraphManager();
+  public static FileManager fm = new FileManager();
+  public static Helpers h = new Helpers();
 
   @Test
   public void planarTest() {
-    gm.breakLine();
+    h.breakLine();
 
-    gm.frameIt("Planarność Grafu", true);
-    // gm.breakLine();
+    h.frameIt("Pakiet: JGraphT", true);
+    h.breakLine();
 
     DefaultUndirectedGraph<String, DefaultEdge> graph = new DefaultUndirectedGraph<String, DefaultEdge>(DefaultEdge.class);
-
-    gm.addVertices(graph, 7);
-
-    gm.addEdges(graph, "v1", "v2", "v3");
-    gm.addEdges(graph, "v3", "v2", "v7");
-
-    gm.frameIt("Lista Sąsiedztwa", false);
-    gm.breakLine();
+    File file = fm.getFileFromResources("input01.txt");
+    graph = fm.prepareGraph(file);
 
     gm.neighboursList(graph);
-    gm.breakLine();
+    h.breakLine();
 
     BoyerMyrvoldPlanarityInspector bmpi = new BoyerMyrvoldPlanarityInspector(graph);
 
-    gm.horizontalLine(35);
+    h.horizontalLine(35);
     System.out.print("   Czy podany graf jest planarny?  ");
     System.out.print(bmpi.isPlanar() ? "TAK" : "NIE");
-    gm.breakLine();
-    gm.horizontalLine(35);
+    h.breakLine();
+    h.horizontalLine(35);
 
-    gm.breakLine();
+    h.breakLine();
   }
 }
